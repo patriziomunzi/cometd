b9af5b01bfd5f4b2dca5b284e493df851e145afa
patriziomunzi <patriziomunzi@gmail.com>
2014-06-14 19:16:04 +0200

    Revert ffd32ce..525160e
    This rolls back to commit ffd32ce3873e12598a634de8fd90a508ff13c6e4.

--- cometd-java/cometd-java-oort/src/main/java/org/cometd/oort/OortConfig.java
+++
@@ -1,8 +0,0 @@
-package org.cometd.oort;
-
-import java.util.Properties;
-
-public interface OortConfig {
-	public void configureCloud(Properties properties, Oort oort) throws OortConfigException;
-	public void destroyCloud() throws OortConfigException;
-}
